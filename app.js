999600c2ee6b21d43785eb05851e9bc2fc6c72be
Mario Finelli <mfinelli@umd.edu>
2015-04-11 14:03:24 -0400

    Add example model creation to app.

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var beacon = require('./routes/beacon');
 var baseroute = require('./routes/index');
+var Sequelize = require('sequelize');
+
+// database connection and models
+var database = require('./config/database');
+var Location = require('./models/location');
+var Beacon = require('./models/beacon');
+var Bus = require('./models/bus');
+var Route = require('./models/route');
+var Stop = require('./models/stop');
+
+database.sync().then(function(){
+    // Here is an example how to add dummy data with a relationship
+    /*b = Beacon.create({phone_number: '5555555555'}).then(function(be){
+        bus = Bus.create({number: "Test"}).then(function(bu){
+            bu.setBeacon(be);
+        });
+    });*/
+});
 
 var app = express();
 app.use(bodyParser.json());
